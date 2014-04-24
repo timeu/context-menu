f1c1cc5bf80b4cf16a3f4724ac5323f5575f93c1
Aki <aleksandar.xyz@gmail.com>
2014-04-23 18:30:18 -0700

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,15 @@
 (function () {
+
   'use strict';
+
   module.exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-vulcanize');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-gh-pages');
 
     grunt.initConfig({
 
-      shell: {
-        mkbuild: {
-          command: 'mkdir dist'
-        }
-      },
 
       vulcanize: {
         dist: {
@@ -24,27 +22,59 @@
         }
       },
 
+      copy: {
+        dist: {
+          files: [
+            {src: 'LICENSE', dest: 'dist/LICENSE'},
+            {src: 'README.md', dest: 'dist/README.md'}
+          ]
+        },
+        demo: {
+          files: [
+            {src: 'dist/floating-menu.html', dest: 'demo/floating-menu.html'},
+            {src: 'index.html', dest: 'demo/index.html'}
+          ]
+        }
+      },
+
+      'gh-pages': {
+        dist: {
+          options: {
+            base: 'dist',
+            branch: 'master',
+            message: 'auto-updating master from dev'
+          },
+          src: '**/*'
+        },
+        demo: {
+          options: {
+            base: 'demo',
+            branch: 'gh-pages',
+            message: 'auto-updating gh-pages from dev'
+          },
+          src: '**/*'
+        }
+      },
+
     });
 
-    // grunt.registerTask('micro', [
-    //   'copy',
-    //   'useminPrepare',
-    //   'concat',
-    //   'cssmin',
-    //   'uglify',
-    //   'wrap',
-    //   'usemin',
-    //   'htmlmin',
-    //   'vulcanize'
-    // ]);
-
     grunt.registerTask('dist', [
-      'shell:mkbuild',
       'vulcanize:dist',
-      // 'htmlmin:dist'
+      'copy:dist',
+      'gh-pages:dist'
+    ]);
+
+    grunt.registerTask('demo', [
+      'vulcanize:dist',
+      'copy:demo',
+      'gh-pages:demo'
     ]);
 
     grunt.registerTask('default', ['dist']);
 
+    grunt.file.mkdir( 'dist' );
+    grunt.file.mkdir( 'demo' );
+
   };
+
 }());
